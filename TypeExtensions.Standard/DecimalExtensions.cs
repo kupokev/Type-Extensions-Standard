4a2a38009fc2436ae35ee9f42fcee3deb49d0482
Kevin Williams <kupo.kev@gmail.com>
2019-09-23 16:57:58 -0500

    initial code push

---
+++ TypeExtensions.Standard/DecimalExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TypeExtensions
+{
+    public static class DecimalExtensions
+    {
+        public static decimal Truncate(this decimal value, int precision)
+        {
+            var amount = (decimal)Math.Pow(10, precision);
+
+            return Math.Truncate(value * amount) / amount;
+        }
+
+        public static decimal? Truncate(this decimal? value, int precision)
+        {
+            if (value == null) return null;
+
+            var amount = (decimal)Math.Pow(10, precision);
+
+            return Math.Truncate((decimal)value * amount) / amount;
+        }
+    }
+}
---
+++ TypeExtensions.Standard/DoubleExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TypeExtensions
+{
+    public static class DoubleExtensions
+    {
+        public static double Truncate(this double value, int precision)
+        {
+            var amount = Math.Pow(10, precision);
+
+            return Math.Truncate(value * amount) / amount;
+        }
+
+        public static double? Truncate(this double? value, int precision)
+        {
+            if (value == null) return null;
+
+            var amount = Math.Pow(10, precision);
+
+            return Math.Truncate((double)value * amount) / amount;
+        }
+    }
+}
